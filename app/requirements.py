904e13b4c9e6da25cdb8389fe8bf76a45802b576
Kitki30 <kitki30@proton.me>
2025-09-23 16:27:58 +0200

    More requirements

--- app/requirements.py
+++ app/requirements.py
@@ -1,6 +1,17 @@
-import modules.cache as cache 
+import modules.cache as cache
+import modules.popup as popup 
 
 def check():
+    # Check extra fucttions
     if not cache.get("rand_extra_func"):
+        popup.show("Your micropython port doesn't support MICROPY_PY_RANDOM_EXTRA_FUNCS!")
         return False
+    
+    # Check version
+    ver = [cache.get("ver_major"), cache.get("ver_minor"), cache.get("ver_patch")]
+    ver_req = [2, 2, 0]
+    if ver < ver_req:
+        popup.show("Ruletta requires Stick firmware version 2.2.0 or higher!")
+        return False
+    
     return True
